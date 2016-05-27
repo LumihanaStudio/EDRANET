47a360f780ff53cae92c9e23784abfeba70d3826
Junseok Oh <kotohana5706@edcan.kr>
2016-05-27 17:36:58 +0900

    User InfoView Activity Implementation

---
+++ app/src/main/java/kr/edcan/edranet/activity/UserInfoViewActivity.java
@@ -0,0 +1,15 @@
+package kr.edcan.edranet.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.edcan.edranet.R;
+
+public class UserInfoViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info_view);
+    }
+}
